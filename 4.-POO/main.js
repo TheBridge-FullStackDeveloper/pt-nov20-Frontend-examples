6ad97b24dff01fdd21c79a11518c3091db10895b
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2021-01-14 20:09:54 +0100

    getters and setters

--- 4.-POO/main.js
+++ 4.-POO/main.js
@@ -25,11 +25,10 @@ class Animal {
 }
 
 class Perro extends Animal {
-	#timeToDeath;
 
 	constructor(name) {
 		super(name, "Perro");
-		this.#timeToDeath = Math.random() * 10000000000;
+		this.timeToDeath = Math.random() * 10000000000;
 	}
 
 	bark() {
@@ -72,3 +71,87 @@ console.log("Perro3", perro3);
 // function hoisting() {
 // 	console.log("Me he ejecutado")
 // }
+
+
+class Termometro {
+	constructor(temperatura) {
+		// K
+		this.temperatura = temperatura;
+		this.count = 0;
+		this.changes = 0;
+		this._color = "black";
+	}
+
+	/*
+		getters
+			Métodos que se comportan como propiedades
+				2 diferencias:
+					1) Que a la hora de llamarlo no pongo los ()
+					2) No puede tener parámetros
+	*/
+	get temperaturaCelsius() {
+		this.count++;
+		return this.temperatura - 273;
+	}
+
+	get temperaturaFarenheit() {
+		return this.temperaturaCelsius * 1.8 + 32;
+	}
+
+	/*
+		setters
+			Métodos que se comportan como propiedades
+				2 diferencias:
+					1) Que a la hora de llamarlo no pongo los ()
+					2) Solo pueden tener 1 parámetro, se va a dar con un =
+	*/
+	set setTemperatura(nuevaTemperatura) {
+		this.changes++;
+		this.temperatura = nuevaTemperatura;
+	}
+
+	get color() {
+		return this._color;
+	}
+
+	set color(newColor) {
+		this._color = newColor	;
+	}
+}
+
+let termometro = new Termometro(295);
+console.log(termometro.temperaturaCelsius);
+console.log(termometro.temperaturaFarenheit);
+
+termometro.setTemperatura = 100;
+
+console.log(termometro.temperaturaCelsius);
+console.log(termometro.temperaturaFarenheit);
+
+console.log(termometro.count);
+console.log(termometro.changes);
+
+
+console.log(termometro.color);
+termometro.color = "red";
+console.log(termometro.color);
+
+
+
+const person = {
+	fechaNacimiento: 2001,
+	get edad() {
+		return new Date().getFullYear() - this.fechaNacimiento;
+	},
+	set edad(nuevaEdad) {
+		this.fechaNacimiento = new Date().getFullYear() - nuevaEdad;
+	},
+
+	crecer: () => {
+
+	}
+}
+
+console.log(person.edad);
+person.edad = 22;
+console.log(person.fechaNacimiento);
