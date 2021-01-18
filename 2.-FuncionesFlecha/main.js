f2d9d8ac8ceff0cc893abaf6d208c0a8c021d8ec
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2021-01-18 21:25:33 +0100

    Reformatted arrow functions theory

--- 2.-FuncionesFlecha/main.js
+++ 2.-FuncionesFlecha/main.js
@@ -1,24 +1,55 @@
-function sum (a, b) {
-	console.log(a + b);
+/*
+	Hasta ahora, siempre habíamos declarado las funciones igual, con la estructura completa:
+		function func_name (parameter1, parameter2, ...) {
+			// Body of the function
+		}
+	Ejemplo:
+*/
+{
+
+	function sum (a, b) {
+		console.log(a + b);
+	}
+
+/*
+	Pero existe una forma más rápida de declarar funciones: las funciones flecha, la cual tiene alguna ventaja más
+	como que mantiene el this del objeto en el que se declaró.
+
+	Su estructura es:
+		(parameter1, parameter2, ...) => {
+			// Body of the function
+		}
+
+	Ejemplo:
+*/
+	const suma = (a, b) => (a + b)
+
+	console.group("function sum(a, b)");
+		sum(1, 2);
+	console.groupEnd();
+	console.group("const suma = (a, b) => {}");
+		console.log(suma(1, 2));
+	console.groupEnd();
+
+	// Cuando solo existe 1 parámetro, puedo eliminar los paréntesis
+	// Cuando el cuerpo de la función solo hace 1 cosa, se pueden quitar las llaves
+	// Si no existen llaves, la función retorna el resultado del body
+	const multiply = a => a * 3;
+
+	// La función multiply retorna (a * 3)
+
+
 }
 
-const suma = (a, b) => (a + b)
-
-
-const multply = a => a * 3;
-
-// function suma (a, b) {
-// 	return (a + b);
-// }
-
-sum(1, 2);
-console.log(suma(1, 2));
-
-
+// Las funciones flecha, son especialmente útiles como callback
 addEventListener("click", () => {
 
 })
 
+
+
+// Otros ejemplos
+console.group("Otros ejemplos");
 
 let p1 = (a, b) => {
 	for(let i = 0; i < b; i++)
@@ -32,3 +63,5 @@ let p2 = (j) => "Hola " + j + "\n";
 let msg = p2("Humano");
 
 console.log(msg);
+
+console.groupEnd();
